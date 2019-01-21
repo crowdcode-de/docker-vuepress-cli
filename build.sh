0846834e020a5dc0e55a5bb0dce47b5a53782bd9
Ingo Düppe <ingo.dueppe@crowdcode.io>
2019-01-21 19:20:59 +0100

    latest  version of vuepress next

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # Build a docker image containing node, npm, vue-cli, and vuepress
 # Set the required vuepress version
-VUEPRESS_VERSION='0.14.8'
+VUEPRESS_VERSION='1.0.0-alpha.32'
 docker build -t "crowdcode/vuepress-cli:${VUEPRESS_VERSION}" --build-arg VUEPRESS_VERSION=${VUEPRESS_VERSION} .
